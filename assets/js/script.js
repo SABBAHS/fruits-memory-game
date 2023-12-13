f4d2decd6909c294b6be1a8c306d3aed68afcfb1
sabbah soroya <sabbahsoroya@hotmail.com>
2023-12-13 18:10:42 +0000

    grids added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,39 @@
-const block = document.querySelectorAll('.card')
+const cards = document.querySelectorAll('.card')
+
+var fruits = ['apple', 'blueberry', 'cherry', 'grape', 'peach', 'pineapple', 'strawberry', 'watermelon'];
+
+
+/**
+ * shuffle and click refresh
+ */
+let numberOfFruits = fruits.length; {
+}
+function shuffle () {
+for (let i = 0; i < numberOfFruits; i++) {
+    var fruits = Math.floor(Math.random()* numberOfFruits);
+}
+function refreshBtn() {
+    document.getElementById("refreshid");
+}
+}
+
+
+/*
+*clicked card
+*/
+/*cards.array.forEach(card => {
+    addEventListener("click", () =>) {
+        card.classList.add("clicked");
+    }
+});
 
 
 /**
  * 
+ * colour change when matched
  */
+
+
+/*
+*timer for 60 second the reshuffle 
+*/
